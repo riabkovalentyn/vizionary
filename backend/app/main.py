2092b64699bef0a0d213f410e3701b5664d3dde3
Valentyn Riabko <rabkovalen@gmail.com>
2024-12-24 16:35:40 +0100

    add frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,19 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from . import crud, models, schemas, database
+
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=database.engine)
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post('/users/')
+    def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+        return crud.create_user(db, user.name, user.email, user.password)
