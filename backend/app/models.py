e66f114b6dc4a5c6430c379273f344331c5e08fc
Valentyn Riabko <rabkovalen@gmail.com>
2024-12-24 15:55:29 +0100

    upt models

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,4 +11,16 @@ class User(Base):
 
     password_hash = Column(String)
 
-    
+
+class DataSource(Base):
+    __tablename__ = 'data_sources'
+
+    id = Column(Integer, primary_key=True, index = True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    source_name = Column(String)
+
+    user = relationship("User", back_populates="data_sources")
+
+User.data_sources = relationship("DataSource", back_populates= "user")
+
+
