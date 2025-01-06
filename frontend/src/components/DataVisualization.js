0536c8f8d1558f869470f7c8480f91aed40e37b1
Valentyn Riabko <riabkovalentyn@gmail.com>
2025-01-06 20:19:35 +0100

    feat: integrate Chart.js for data visualization in DataVisualization component

--- frontend/src/components/DataVisualization.js
+++ frontend/src/components/DataVisualization.js
@@ -1,24 +1,38 @@
 import React from "react";
 import { Bar } from 'react-chartjs-2'
-import { Chart as ChartJS,  } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip,Legend } from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const DataVisaulization = ({ data }) => {
     const chartData = {
         labels: data.map(item => item.date),
         datasets: [{
-            labels: 'Data Over Time',
+            labels: 'Dataset ',
             data: data.map(item => item.value),
-            boerderColor: 'rgb(75, 192, 192)',
+            borderColor: 'rgb(75, 192, 192)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
             fill: false,
 
         },
         ],
     };
-    
+    const options = {
+        responsive: true,
+        plugins: {
+            legend:{
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Data Visaulization',
+            },
+        },
+    }    
     return(
         <div>
       <h2>Data Visualization</h2>
-      <Line data={chartData} />
+      <Bar data={chartData} options={options} />
         </div>
     );
 };
