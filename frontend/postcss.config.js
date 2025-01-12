3e7bc24534218af8ead71fec607b2d4ee23e76fd
Valentyn Riabko <riabkovalentyn@gmail.com>
2025-01-12 22:09:44 +0100

    feat: enhance WelcomePage with dark mode toggle and add custom colors to Tailwind CSS

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -2,6 +2,16 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  theme:{
+    extend: {
+      colors:{
+        'cyberpunk-pink': '#FF007F',
+        'cyberpunk-purple': '#8a2be2',
+        'cyberpunk-green': '#00FF00',
+        'cyberpunk-blue': '#00eaff'
+      }
+    }
+  },
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
--- frontend/src/components/WellcomePage.js
+++ frontend/src/components/WellcomePage.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';  
+import { Button } from 'react-bootstrap';
+import { Switch } from '@headlessui/react';  
 
 
 const WelcomePage = () =>{
+    const [darkMode, setDarkMode] = useState(false);
+
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1>Welcome to Vizionary</h1>
+        <div classname={`flex flex-col items-center justify-center min-h-screen ${darkMode ? 'bg-gray-900 text-white': 
+            'bg-gray-100 text-black'}`} style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1 className="text-6xl font-bold mb-4 animate-pulse">Welcome to Vizionary</h1>
             <p>Your one-stop solution for data visualization.</p>
         <Button variant="contained" color="primary" component={Link} to="/data-visualization">
          Go to Data Visualization
